4480896079e83e7dbc9d102b00bd8b56a417ac87
Aditya <adityanagare09@gmail.com>
2024-07-20 18:22:13 +0530

    loading added to home

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -6,11 +6,13 @@ import {PROXY_URL} from '../utils/constants'
 const Posts = ({feedType , userProfile , username}) => {
 
 	const [POSTS, setPOSTS] = useState([]);
+	const [loading, setLoading] = useState(false);
 
 	useEffect(() => {
 	  const getPosts = async ()=>{
+		setLoading(true);
 		try {
-			console.log(feedType);
+			// console.log(feedType);
 			const url = ()=>{
 				if (feedType === "following") {
 					return `${PROXY_URL}/api/posts/following`
@@ -33,10 +35,12 @@ const Posts = ({feedType , userProfile , username}) => {
 
 			const data = await response.json();
 			if (!response.ok) {
+				setLoading(false);
 				toast(data?.error);
 				return;
 			}
 			// console.log(data);
+			setLoading(false);
 			setPOSTS(data);
 
 	
@@ -54,11 +58,15 @@ const Posts = ({feedType , userProfile , username}) => {
 		<>
 			{!isLoading && POSTS?.length === 0 && <p className='text-center my-4'>No posts in this tab. Switch 👻</p>}
 			{!isLoading && POSTS && (
-				<div>
+				
+					loading ? <div className='grid col-span-5 place-items-center min-h-screen'>
+					<div className="w-8 h-8 place-self-center border-4 border-gray-400 border-t-green-800 rounded-full animate-spin"></div>
+				  </div> : <div>
 					{POSTS.map((post) => (
 						<Post key={post._id} poster={post} />
 					))}
 				</div>
+				
 			)}
 		</>
 	);
